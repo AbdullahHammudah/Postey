28e6f95726b44a3bb465cd3a6a9ac79e998455f7
Abdullah Hammudah <a.m.a.h97126@gmail.com>
2022-03-03 15:04:51 +0200

    third commit

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts', views.records, name='posts')
+    path('posts', views.records, name='posts'),
+    path('posts', views.search, name='search')
 ]
--- api/views.py
+++ api/views.py
@@ -1,38 +1,95 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.response import Response
-from api.models import Post
-from api.serializers import PostSerializer
+from app.apiResponse import prepareResponse
+from api.models import *
+from api.serializers import *
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 
 
 
-# Create your views here.
-@api_view(['GET'])
-def records(request):
-    print(request)
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
+
+class posts(viewsets.ModelViewSet):
+
+    def records(self, request):
+        """
+        - Read all posts, Search, Sort, and page collection of posts
+        params: 'page', 'search', 'order', 'type'
+        """
+        posts = Post.objects.exclude(status=StatusChoises.DELETED)
+        sort_by = request.GET.get('type')
+        page = int(request.GET.get('page')) if 'page' in request.GET else 1
+        startRecord = (page -1) * settings. PER_PAGE
+        endRecord = startRecord + settings.PER_PAGE
+
+        # Search 
+        if 'search'in request.GET:
+            query = request.GET.get('search')
+            vector = SearchVector('title', 'description')
+            searchQuery = SearchQuery(query)
+
+            posts = Post.objects\
+                .exclude(status=StatusChoices.DELETED)\
+                .annotate(rank=SearchRank(vector,searchQuery))\
+                .filter('rank__gte=0.001')\
+                .order_by('-rank')
+
+        # Sorting
+        if 'order' in request.GET == 'desc':
+            posts = posts.exclude(status=StatusChoices.DELETED).all().order_by(sort_by.desc())
+
+        serializer = PostSerializer(posts.all()[startRecord:endRecord], many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def single_record(request,id):
-    if request.method == 'GET':
-        post = Post.objects.get(pk=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+    def record(self, request, id):
+        """
+        GET post by id
+        """
+        post = Post.objects.exclude(
+            status=StatusChoises.DELETED).filter(id=id).first()
+        if post:
+            serializer = PostSerializer(post, many=False)
+            return Response(prepareResponse({'total':1}, serializer.data))
+        else:
+            return Response(prepareResponse({'status':404, 'instance':request.get_full_path()}, {}, False), 404)
+    
+    def create(self, request):
+        """
+        Create new post
+        """ 
+        post = Post()
+        for attr, value in request.data.items():
+            if attr not in Post.protected():
+                setattr(post, attr, value)
+        try:
+            post.save()
+            serializer = PostSerializer(post, many=False)
+        except Exception as error:
+            return Response(prepareResponse({'status':422, 'instance':request.get_full_path()}, error, False), 422)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
-@api_view(['POST'])
-def add_record(request):
-    serializer = PostSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
+    def update(self, request, id):
+        post = Post.objects.exclude(
+            status=StatusChoises.DELETED).filter(id=id).first()
+        if post:
+            for attr, value in request.data.items():
+                if attr not in Post.protected():
+                    setattr(post, attr, value)
+            try:
+                post.save()
+                serializer = PostSerializer(post, many=False)
+                return Response(prepareResponse({'total':1},serializer.data), 201)
+            except Exception as error:
+                return Response(prepareResponse({'status':422, 'instance':request.get_full_path()}, error, False), 422)
+        else:
+            return Response(prepareResponse({"status": 404, "instance": request.get_full_path()}, {}, False), 404)
 
-@api_view(['GET'])
-def search(request):
-    query = request.GET.get('q')
-    posts = Post.objects.filter(query)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+    def delete(self, request, id=None):
+        post = Post.objects.filter(id=id).exclude(
+            status=StatusChoices.DELETED).first()
+
+        if post:
+            setattr(post, status, StatusChoises.DELETED)
+
         
